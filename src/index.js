076163bd0b2f18496d1077ce1d59ba1794e68dd9
shamrep <shamrep@gmail.com>
2023-10-28 00:01:27 +0300

    solve

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+n = Math.abs(n); 
+let reversed = 0;
+
+while (n !== 0) {
+  reversed = reversed * 10 + n % 10;
+  n = Math.floor(n / 10);
+}
+
+return reversed; 
 }
